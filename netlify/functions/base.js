8c188227079a2fc99ad79d2567ac048e95375ce8
isaac Groisman <grisman13@hotmail.com>
2023-01-15 23:03:47 +0200

    netlify setup

---
+++ netlify/functions/base.js
@@ -0,0 +1,27 @@
+const Airtable = require('airtable')
+
+const db = new Airtable({
+  apiKey: process.env.AIRTABLE_TOKEN,
+}).base(process.env.AIRTABLE_BASE_ID)
+
+const headers = {
+  /* Required for CORS support to work */
+  'Access-Control-Allow-Origin': '*',
+  /* Required for cookies, authorization headers with HTTPS */
+  'Access-Control-Allow-Credentials': true,
+   /* Para visualizar mejor el texto en JSON */
+   "content-type": "application/json",
+}
+
+exports.handler = async function (evt) {
+  const articles = await db('articles').select().all()
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({
+      //hello: 'world',
+      articles,
+    }),
+  }
+}
